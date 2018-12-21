650dbf0b04757fbe1eeb0eaf59ee0d3700402f3b
LewisMcReu <wim.creuwels@hotmail.com>
2018-12-21 15:43:29 +0100

    Github Pages #1

---
+++ docs/precache-manifest.6c72871ee8f5ae26e1235d0bfb604458.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "240bedf12e22e46d5eab",
+    "url": "/static/css/main.41566d28.chunk.css"
+  },
+  {
+    "revision": "240bedf12e22e46d5eab",
+    "url": "/static/js/main.240bedf1.chunk.js"
+  },
+  {
+    "revision": "217e004b83d12cfd2d72",
+    "url": "/static/js/1.217e004b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c2661bc23e9bc59c6950664ba65129d",
+    "url": "/index.html"
+  }
+];
